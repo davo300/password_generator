b03142d47488a90a808cc550ad70d0a5e2f1faaf
Matt Davies <matt.davies400@gmail.com>
2025-09-13 13:35:07 -0400

    Initial commit

---
+++ password.js
@@ -0,0 +1,14 @@
+function generatePassword() {
+    const length = document.getElementById('length').value;
+    const passwordResult = document.getElementById('passwordResult');
+
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?";
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    passwordResult.textContent = password;
+}
